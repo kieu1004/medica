3cca653eb174782f5b0b6f8b66c6b23f8ba9605f
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-07-20 13:07:50 +0700

    color

---
+++ medica/src/global/styles.js
@@ -0,0 +1,57 @@
+export const colors = {
+    primary_1: "#439BE8",
+    primary_2: "#BBE7FC",
+    primary_3: "#5BCCCF",
+    primary_4: "#ADE8E8",
+
+    secondary_1: "#2D3E4E",
+    secondary_2: "#617A8F",
+    secondary_3: "#949A9F",
+    secondary_4: "#7CAFC9",
+
+    neutral_1: "#161616",
+    neutral_2: "#646464",
+    neutral_3: "#BEBEBE",
+    neutral_4: "#F2F2F2",
+    neutral_5: "#F2F2F2",
+
+    transparent: "#00000000",
+    background: "#f5f5f5",
+    statusbar: "#00000000",
+
+    text_fuzz3: "#333",
+    text_fuzz6: "#666",
+    text_fuzz7: "#777",
+    text_white: "#000",
+
+}
+
+export const parameters = {
+    headerHeight: 40
+}
+
+export const fonts = {
+    ios: {
+        regular: 'System',
+        light: 'System',
+        lightItalic: 'System',
+        bold: 'System',
+        boldItalic: 'System',
+        black: 'System',
+        blackItalic: 'System',
+    },
+    android: {
+        regular: 'Roboto',
+        italic: 'Roboto-Italic',
+        thin: 'Roboto-Thin',
+        thinItalic: 'Roboto-ThinItalic',
+        light: 'Roboto-Light',
+        lightItalic: 'Roboto-LightItalic',
+        medium: 'Roboto-Medium',
+        mediumItalic: 'Roboto-MediumItalic',
+        bold: 'Roboto-Bold',
+        boldItalic: 'Roboto-BoldItalic',
+        condensed: 'RobotoCondensed-Regular',
+        condensedItalic: 'RobotoCondensed-Italic',
+    }
+}
